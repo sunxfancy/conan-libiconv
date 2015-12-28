9da5d0b4163b522ea104d5ce3983e094a5737af3
Luis Martinez <lasote@gmail.com>
2015-12-28 13:24:57 +0100

    Windows fixes

--- test/example.cpp
+++ test/example.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     char * pOut = ( char*)dst;
  
     iconv_t conv = iconv_open("UTF-8","CP1250");
-    iconv(conv, &pIn, &srclen, &pOut, &dstlen);
+    iconv(conv, (const char **) &pIn, &srclen, &pOut, &dstlen);
     iconv_close(conv);
  
     fprintf(stderr,"out: %s\n",dst);
