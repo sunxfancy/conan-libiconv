3c6dc9aa51818d874e988ea4b102a169a70ff3a6
Luis Martinez de Bartolome <lasote@gmail.com>
2016-01-09 18:04:15 +0100

    Added build with docker gcc 5.3

--- build_with_docker.py
+++ build_with_docker.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
         
-    for gcc_version in ["4.6", "4.8", "4.9", "5.2"]:
+    for gcc_version in ["4.6", "4.8", "4.9", "5.2", "5.3"]:
         image_name = "lasote/conangcc%s" % gcc_version.replace(".", "")
         os.system("sudo docker pull %s" % image_name)
         curdir = os.path.abspath(os.path.curdir)
