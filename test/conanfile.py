b0e09cc1aa42acb5ac370da9fab080c3f0a451ee
西风逍遥游 <sun_xfancy@163.com>
2017-11-16 21:48:26 -0800

    fix

--- test/conanfile.py
+++ test/conanfile.py
@@ -17,7 +17,7 @@ class DefaultNameConan(ConanFile):
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "libiconv/1.14.1@%s/%s" % (username, channel)
+    requires = "libiconv/1.14.2@%s/%s" % (username, channel)
 
     def build(self):
         cmake = CMake(self.settings)
